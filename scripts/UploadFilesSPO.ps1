8f82d30222d501a193b959d261f3d3dacc4ba3ec
Tiesurgical <73509951+tiesurgicalDevOps@users.noreply.github.com>
2020-11-08 17:46:33 +1100

    UploadFilesSPO upload

---
+++ scripts/UploadFilesSPO.ps1
@@ -0,0 +1,18 @@
+﻿
+Set-ExecutionPolicy Unrestricted
+Set-ExecutionPolicy -ExecutionPolicy AllSigned -Scope CurrentUser
+Get-ExecutionPolicy -List
+#First time tasks
+#Install-Module SharePointPnPPowerShellOnline 
+$URL = "https://tiesurgical.sharepoint.com/sites/QA/"
+$Creds = get-credential
+#Add-PnPStoredCredential -Name $URL -Username $Creds.UserName -Password $Creds.Password
+Get-PnPStoredCredential -Name $Creds -Type PSCredential
+Connect-PnPOnline -Url $URL -Credentials  $Creds
+
+$Files = Get-ChildItem "Z:\JobSystem\PowerApp Data\"
+foreach($File in $Files){
+    #$File = $Files[0]
+    Add-PnPFile -Folder "Shared Documents/General" -Path $File.FullName
+   # Write-Host "Uploading csv import" $File.FullName
+}
