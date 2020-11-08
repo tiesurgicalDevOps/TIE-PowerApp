4dc63ff53b92c6ec4d1461cc6474bc8ebff2668d
Tiesurgical <73509951+tiesurgicalDevOps@users.noreply.github.com>
2020-11-08 23:07:10 +1100

    Update UploadFilesSPO.ps1

--- Scripts/UploadFilesSPO.ps1
+++ Scripts/UploadFilesSPO.ps1
@@ -2,7 +2,7 @@
 Set-ExecutionPolicy Unrestricted
 Set-ExecutionPolicy -ExecutionPolicy AllSigned -Scope CurrentUser
 Get-ExecutionPolicy -List
-#First times NEW
+#First times 
 #Install-Module SharePointPnPPowerShellOnline 
 $URL = "https://tiesurgical.sharepoint.com/sites/QA/"
 $Creds = get-credential
